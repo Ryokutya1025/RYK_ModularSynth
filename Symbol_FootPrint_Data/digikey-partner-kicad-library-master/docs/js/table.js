a856f193a3c4142dabd7f89ca689c2c1f852e402
Ryokutya1025 <ryokutya2000@gmail.com>
2026-06-05 04:59:53 +0900

    fix 16pin layout

--- Symbol_FootPrint_Data/digikey-partner-kicad-library-master/docs/js/table.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#listing_table').DataTable(
-    {
-        "order": [[6, 'asc']],
-        "columnDefs": [{"targets": [0,1], "orderable": false}]
-    });
-});
